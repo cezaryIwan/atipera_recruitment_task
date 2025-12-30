73994e885f95b34bcc8039e24e6fad82b2a7f7af
Cezary Iwan <ycezary.iwan@gmail.com>
2025-12-30 22:57:39 +0100

    Initial commit

---
+++ task/src/main/java/com/atipera/task/Controller.java
@@ -0,0 +1,11 @@
+package com.atipera.task;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("path")
+public class Controller {
+
+
+}
